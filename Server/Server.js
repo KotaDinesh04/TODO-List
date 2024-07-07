019213cb9ad739fbdb6c765a30ae36552875e012
Dinesh Kota <mail.kotadinesh@gmail.com>
2024-07-07 14:32:43 +0530

    Basic Backend Setup Done(Structure)

---
+++ Server/Server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//MongoDB Connection->
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=> {
+    console.log("Connected to Database");
+}).catch(err => {
+    console.log("Error connecting to Database :",err);
+});
+
+// Define a simple route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
