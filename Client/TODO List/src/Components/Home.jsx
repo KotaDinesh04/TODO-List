722e6e2ccd3eb87e38d12c65d5b06738767145ad
Dinesh Kota <mail.kotadinesh@gmail.com>
2024-07-09 23:22:33 +0530

    Fixed a small bug

--- Client/TODO List/src/Components/Home.jsx
+++ Client/TODO List/src/Components/Home.jsx
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default function Home({email}) {
-  const [data,setData] = useState(null);
+export default function Home({ email }) {
+  const [data, setData] = useState(null);
 
-  // logic to fill the data of logged in user:
-  const getArr = async ()=>{
+  const getArr = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/api/getuserdata',{
+      const response = await axios.post('http://localhost:5000/api/getuserdata', {
         emailId: email
-      })
+      });
       setData(response.data);
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   }
+
   useEffect(() => {
     getArr();
   }, []);
 
   console.log(data);
+
   return (
     <div>
-      <h1>Welcome {data.name}</h1>
+      {data && <h1>Welcome {data.name}</h1>}
     </div>
   );
 }
